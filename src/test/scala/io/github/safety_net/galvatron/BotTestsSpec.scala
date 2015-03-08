79eb221ec4d6830b2f0d45c82923d8bd0ad46d29
Joe <josephpkirwin@gmail.com>
2015-03-07 19:39:32 -0800

    refactored to use lazyConstructor

--- src/test/scala/io/github/safety_net/galvatron/BotTestsSpec.scala
+++ src/test/scala/io/github/safety_net/galvatron/BotTestsSpec.scala
@@ -1,28 +1,28 @@
 package io.github.safety_net.galvatron
 
-import java.io.File
-
 import org.specs2.mutable._
 import scala.io.Source
 
 class BotTestsSpec extends Specification {
 
+  lazy val lzBotTests = {
+    // TODO gitcrypt
+    // file with twitterKeys not to be commited!!
+    // structure
+    // ConsumerKey \n
+    // ConsumerSecret \n
+    // AccessToken \n
+    // AccessTokenSecret
+    val filename = "src/test/resources/twitterKeys.keys"
+    val keyLines = Source.fromFile(filename).getLines().toList
+    new BotTests(keyLines(0),keyLines(1),keyLines(2),keyLines(3))
+  }
 
   "friendRatioCheck" should{
     "returns results for valid user" in {
-      // TODO gitcrypt
-      // file with twitterKeys not to be commited!!
-      // structure
-      // ConsumerKey \n
-      // ConsumerSecret \n
-      // AccessToken \n
-      // AccessTokenSecret
-      val filename = "src/test/resources/twitterKeys.keys"
-      val keyLines = Source.fromFile(filename).getLines().toList
-      val botTests = new BotTests(keyLines(0),keyLines(1),keyLines(2),keyLines(3))
       // SomeBot user
       val user = "dmoran850"
-      val result = botTests.friendRatioCheck(user)
+      val result = lzBotTests.friendRatioCheck(user)
       println(result)
       println()
       (result > 0) must beTrue
